cc379d216e7f5be23e6c5af544ce84dd9fd401e9
shahnawaz-bidgely <shahnawaz@bidgely.com>
2023-10-15 16:49:58 +0530

    Initialized project

---
+++ src/main/java/com/avenger/bflow/BflowApplication.java
@@ -0,0 +1,14 @@
+package com.avenger.bflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BflowApplication.class, args);
+		System.out.println("Welcome to Bidgely offical stack overflow");
+	}
+
+}
---
+++ src/test/java/com/avenger/bflow/BflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avenger.bflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
